1001e76dfbe530e9ce5f1d966b7483755b48bcf1
pmconne <22944042+pmconne@users.noreply.github.com>
2023-12-07 13:57:40 -0500

    decode input

--- 7/main.js
+++ 7/main.js
@@ -1,3 +1,8 @@
+const sampleInput = `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`;
 
 // Remap so that lexical sort puts the highest-valued cards first
 function encodeHand(hand) {
@@ -8,6 +13,16 @@ function encodeHand(hand) {
   return [...hand].map((x) => remap[x]).join("");
 }
 
+function decodeHands(input) {
+  const hands = [];
+  for (const line of input.split("\n")) {
+    const parts = line.split(" ");
+    hands.push({ cards: encodeHand(parts[0]), bid: Number.parseInt(parts[1], 10) });
+  }
+
+  return hands;
+}
+
 // 0: 5 of a kind
 // 1: 4 of a kind
 // 2: full house
@@ -16,13 +31,15 @@ function encodeHand(hand) {
 // 5: 1 pair
 // 6: all different
 function categorizeHand(hand) {
+  // console.log(`${hand} => ${encodeHand(hand)}`);
+
   const cardCounts = { };
   for (const card of [...hand])
     cardCounts[card] = (cardCounts[card] ?? 0) + 1;
 
-  console.log(JSON.stringify(cardCounts));
+  // console.log(JSON.stringify(cardCounts));
   const sorted = Array.from(Object.values(cardCounts)).sort((x, y) => y - x);
-  console.log(sorted);
+  // console.log(sorted);
 
   switch (sorted[0]) {
     case 5: return 0;
@@ -34,10 +51,24 @@ function categorizeHand(hand) {
   }
 }
 
-console.log(categorizeHand("33333"));
-console.log(categorizeHand("3333A"));
-console.log(categorizeHand("333AA"));
-console.log(categorizeHand("33AB3"));
-console.log(categorizeHand("33BAA"));
-console.log(categorizeHand("A33BC"));
-console.log(categorizeHand("3ABCD"));
+function sortByRank(hands) {
+  const groups = [[], [], [], [], [], [], []];
+  for (const hand of hands)
+    groups[categorizeHand(hand.cards)] = hand;
+
+  for (const group of groups)
+    group.sort();
+
+  return Array.prototype.concat(...groups);
+}
+
+// Prints 0..6
+// console.log(categorizeHand("33333"));
+// console.log(categorizeHand("3333A"));
+// console.log(categorizeHand("333AA"));
+// console.log(categorizeHand("33AK3"));
+// console.log(categorizeHand("33KAA"));
+// console.log(categorizeHand("A33KQ"));
+// console.log(categorizeHand("3AKQJ"));
+
+console.log(JSON.stringify(decodeHands(sampleInput)));
