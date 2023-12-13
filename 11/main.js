6f742b5085f67a3b694316bdcebb8dcc80294af4
pmconne <22944042+pmconne@users.noreply.github.com>
2023-12-13 10:07:36 -0500

    compute shortest path distances

--- 11/main.js
+++ 11/main.js
@@ -40,9 +40,33 @@ function parseGalaxies(input) {
   return galaxies;
 }
 
+function computeShortestPathLength(start, end) {
+  const dx = end.x - start.x;
+  const dy = end.y - start.y;
+  if (dx === 0 && dy === 0)
+    return 0;
+
+  const next = { ...start };
+  if (Math.abs(dx) > Math.abs(dy))
+    next.x += dx > 0 ? 1 : -1;
+  else
+    next.y += dy > 0 ? 1 : -1;
+
+  return 1 + computeShortestPathLength(next, end);
+}
+
 function part1(input) {
   const galaxies = parseGalaxies(input);
-  console.log(galaxies);
+  let totalDistance = 0;
+  for (let i = 0; i < galaxies.length; i++) {
+    for (let j = i + 1; j < galaxies.length; j++) {
+      const distance = computeShortestPathLength(galaxies[i], galaxies[j]);
+      totalDistance += distance;
+      console.log(`${i+1} => ${j+1} = ${distance}`);
+    }
+  }
+
+  return totalDistance;
 }
 
 console.log(part1(sampleInput1));
