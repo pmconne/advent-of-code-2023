6453f61e5d072b9a52ddcddf11f77d112aac700c
pmconne <22944042+pmconne@users.noreply.github.com>
2023-12-14 08:46:05 -0500

    12 p1 sample input passes

--- 12/main.js
+++ 12/main.js
@@ -17,10 +17,40 @@ function parse(input) {
   return map;
 }
 
+function collectArrangements(springs, output) {
+  for (let i = 0; i < springs.length; i++) {
+    if ("?" === springs[i]) {
+      collectArrangements(springs.slice(0, i) + "." + springs.slice(i + 1), output);
+      collectArrangements(springs.slice(0, i) + "#" + springs.slice(i + 1), output);
+      return;
+    }
+  }
+
+  output.push(springs);
+}
+
+function countDamagedGroups(springs) {
+  const groups = springs.split(".").filter((x) => x.length > 0).map((x) => x.length);
+  return groups;
+}
+
+function countArrangements(row) {
+  const arrangements = [];
+  collectArrangements(row.springs, arrangements);
+  let numMatches = 0;
+  for (const arrangement of arrangements) {
+    const groups = countDamagedGroups(arrangement);
+    if (groups.length === row.groups.length && groups.every((value, index) => value === row.groups[index]))
+      ++numMatches;
+  }
+
+  console.log(`${row.springs} => ${numMatches}`);
+  return numMatches;
+}
+
 function part1(input) {
   const map = parse(input);
-  console.log(map);
-  return 0;
+  return map.map((row) => countArrangements(row)).reduce((prev, cur) => prev + cur, 0);
 }
 
 console.log(part1(sampleInput));
