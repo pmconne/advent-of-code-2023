5d0528dbbef0b66e22adde34e68c8700256899d6
pmconne <22944042+pmconne@users.noreply.github.com>
2023-12-16 03:45:28 -0500

    16 wip

---
+++ 16/main.js
@@ -0,0 +1,98 @@
+
+// Backslashes in input, how fun...
+const sampleInput = String.raw
+`.|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|....`;
+
+function parseMap(input) {
+  return input.split("\n").map((row) => {
+    return Array.from(row).map((symbol) => {
+      return { symbol };
+    });
+  });
+}
+
+function add(vec1, vec2) {
+  return {
+    x: vec1.x + vec2.x,
+    y: vec1.y + vec2.y,
+  };
+}
+
+function tracePath(map, pos, dir, beamGeneration = 0) {
+  while (true) {
+    const row = map[pos.y];
+    const tile = row ? row[pos.x] : undefined;
+    if (!tile)
+      return;
+    
+    tile.hot = true;
+    console.log(`${beamGeneration} ${tile.symbol} p=(${pos.x},${pos.y}) d=(${dir.x},${dir.y})`);
+    switch (tile.symbol) {
+      case ".":
+        pos = add(pos, dir);
+        break;
+      case "\\":
+      case "/":
+        const tmp = dir.x;
+        dir.x = dir.y;
+        dir.y = tmp;
+        if ("/" === tile.symbol) {
+          dir.x *= -1;
+          dir.y *= -1;
+        }
+
+        pos = add(pos, dir);
+        break;
+      case "-": {
+        if (dir.x !== 0) {
+          pos = add(pos, dir);
+          break;
+        }
+
+        const dir0 = { y: 0, x: -1 };
+        const dir1 = { y: 0, x: 1 };
+        tracePath(map, add(pos, dir0), dir0, beamGeneration + 1);
+        tracePath(map, add(pos, dir1), dir1, beamGeneration + 1);
+        return;
+      }
+      case "|": {
+        if (dir.y !== 0) {
+          pos = add(pos, dir);
+          break;
+        }
+
+        const dir0 = { x: 0, y: -1 };
+        const dir1 = { x: 0, y: 1 };
+        tracePath(map, add(pos, dir0), dir0, beamGeneration + 1);
+        tracePath(map, add(pos, dir1), dir1, beamGeneration + 1);
+        return;
+      }
+      default:
+        throw new Error(`Unexpected symbol ${tile.symbol}`);
+    }
+  }
+}
+
+function prettify(map) {
+  return map.map((row) => row.map((tile) => tile.hot ? "#" : tile.symbol).join("")).join("\n");
+}
+
+function part1(input) {
+  const map = parseMap(input);
+  try {
+    tracePath(map, { x: 0, y: 0 }, { x: 1, y: 0 });
+  } catch (_) { }
+  console.log(prettify(map));
+  return map.reduce((accum, row) => row.reduce((accum, tile) => accum + tile.hot ? 1 : 0, accum), 0);
+}
+
+console.log(part1(sampleInput));
